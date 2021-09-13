3b2973806f7f4499c54dea824be628ad86d7da04
Aanchal2325ch <81634944+Aanchal2325ch@users.noreply.github.com>
2021-09-13 16:10:34 +0530

    Create project.groovy

---
+++ etc/jenkins_jobs/project.groovy
@@ -0,0 +1,15 @@
+pipelineJob('dsl-demo') {
+  definition {
+    cpsScm {
+      scm {
+        git {
+          remote {
+            url('https://github.com/Aanchal2325ch/jenkins_jobBuilder.git')
+          }
+          branch('*/main')
+        }
+      }
+      lightweight()
+    }
+  }
+}
